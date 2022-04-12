2f63a1b9cb55a1e4e06345d77574e6b73c4d765d
Kenny <kennysalazar3197@gmail.com>
2022-04-11 20:23:43 -0500

    Created basic strcuture for the App component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { Outlet, useLocation, Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { BsFillMicFill, BsFillGearFill } from 'react-icons/bs';
+import { IoIosArrowBack } from 'react-icons/io';
+import './App.scss';
 
-function App() {
+const App = () => {
+  const { pathname } = useLocation();
+
+  const renderHeaderTitle = () => {
+    switch (pathname) {
+      case '/':
+        return 'home';
+      case '/details':
+        return 'details';
+      default:
+        return 'unknown';
+    }
+  };
+
+  const renderBackBtn = () => {
+    switch (pathname) {
+      case '/':
+        return false;
+      case '/details':
+        return (
+          <Link to="/" className="fs-5">
+            <IoIosArrowBack />
+          </Link>
+        );
+      default:
+        return (
+          <Link to="/" className="fs-5">
+            <IoIosArrowBack />
+          </Link>
+        );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <Navbar as="header">
+        <Container fluid="md">
+          {renderBackBtn()}
+          <h1 className="mx-auto my-0">{renderHeaderTitle()}</h1>
+          <div>
+            <button type="button" className="button-icon">
+              <BsFillMicFill />
+            </button>
+            <button type="button" className="button-icon">
+              <BsFillGearFill />
+            </button>
+          </div>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import './index.scss';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/details" element={<h1>Details</h1>} />
+          <Route path="*" element={<h2 className="text-center my-5">Nothing here!</h2>} />
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();

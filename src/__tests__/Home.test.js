083b9b1dfc132f2761f91bbe5bd3dec4be52626c
Kenny <kennysalazar3197@gmail.com>
2022-04-16 01:56:51 -0500

    Added tests for the Home component

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,27 +1,27 @@
 import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
-import App from '../App';
-
-jest.mock('../redux/filter/filter');
-jest.mock('../redux/home/home');
+import Home from '../components/Home';
 
 const reducerMock = () => ({
-  path: { header: 'test_header' },
+  home: { status: 'FETCHING_GLOBAL_METRICS_FAILED', error: 'test_error' },
 });
 
 const store = createStore(reducerMock, applyMiddleware(thunk));
 
-describe('Tests for the App component', () => {
-  test('The component should render', () => {
+describe('Tests for the Home component', () => {
+  test('The component should display an error when fetching fails', () => {
     const { getByText } = render(
       <Provider store={store}>
-        <App />
+        <Router>
+          <Home />
+        </Router>
       </Provider>,
     );
 
-    expect(getByText(/test_header/i)).toBeInTheDocument();
+    expect(getByText(/test_error/i)).toBeInTheDocument();
   });
 });

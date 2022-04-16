9aca06c00dd94095109df16ea7af8afeb02bc331
Kenny <kennysalazar3197@gmail.com>
2022-04-16 10:10:11 -0500

    Added tests for the CountriesList component

--- src/__tests__/Filter.test.js
+++ src/__tests__/Filter.test.js
@@ -6,7 +6,37 @@ import { createStore, applyMiddleware } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import Filter from '../components/Filter';
 
-describe('Tests for the Home component', () => {
+const countriesDataMock = {
+  status: 'FETCHING_COUNTRIES_METRICS_SUCCEEDED',
+  continentMetrics: {
+    europe: {
+      cases: 'test_cases',
+      deaths: 'test_deaths',
+    },
+    northAmerica: {
+      cases: '1',
+      deaths: '0',
+    },
+    southAmerica: {
+      cases: '12',
+      deaths: '11',
+    },
+    africa: {
+      cases: '2',
+      deaths: '4',
+    },
+    asia: {
+      cases: '7',
+      deaths: '10',
+    },
+    oceania: {
+      cases: '5',
+      deaths: '8',
+    },
+  },
+};
+
+describe('Tests for the Filter component', () => {
   test('The component should display a loading icon when starting to fetch data', () => {
     const reducerMock = () => ({
       filter: { status: 'FETCHING_COUNTRIES_METRICS' },
@@ -40,5 +70,22 @@ describe('Tests for the Home component', () => {
     );
 
     expect(getByText(/test_error/i)).toBeInTheDocument();
+  });
+  test('The component should render and displays a list of continents when the fetcthing succeeds', () => {
+    const reducerMock = () => ({
+      filter: countriesDataMock,
+    });
+
+    const store = createStore(reducerMock, applyMiddleware(thunk));
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <Router>
+          <Filter />
+        </Router>
+      </Provider>,
+    );
+
+    expect(getByText(/test_cases/i)).toBeInTheDocument();
   });
 });
